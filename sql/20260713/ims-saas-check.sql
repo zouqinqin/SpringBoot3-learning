173417267f89a8269d5a32c612e2a3a124bb33ff
zouqinqin <zqq1161709883@gmail.com>
2026-07-13 14:42:11 +0800

    测试代码和sql2

--- sql/20260713/ims-saas-check.sql
+++ sql/20260713/ims-saas-check.sql
@@ -42,3 +42,14 @@ create table rec_finance_approval_approver
 
 create index idx_approval on rec_finance_approval_approver (approval_id);
 
+
+v_qinqinzou(邹勤勤) 邀请您参加腾讯会议
+会议主题：v_qinqinzou(邹勤勤)的快速会议
+会议时间：2026/07/13 14:40-15:40 (GMT+08:00) 中国标准时间 - 北京
+
+点击链接直接加入会议：
+https://meeting.tencent.com/dm/9Ck9D4iW1gFu
+
+#腾讯会议：908-115-983
+
+复制该信息，打开手机腾讯会议即可参与
