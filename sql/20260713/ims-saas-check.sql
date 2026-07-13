56c9d5abccacd39478a998cb97689ad1a882a371
v_qinqinzou <v_qinqinzou@tencent.com>
2026-07-13 12:00:26 +0800

    feat: 初始化项目结构并添加字典枚举和自定义 starter 模块

--- sql/20260713/ims-saas-check.sql
+++ sql/20260713/ims-saas-check.sql
@@ -3,15 +3,17 @@ create table rec_finance_approval
 (
     id              bigint unsigned auto_increment primary key,
     approval_no     varchar(32)                           not null comment '单据编号',
-    initiator       varchar(64)                           not null comment '提交审批人',
-    initiate_time   datetime    default CURRENT_TIMESTAMP not null,
     approval_status varchar(32) default 'PENDING'         not null comment '审批状态',
     bill_count      int         default 1                 not null comment '账单数量',
-    approver_id     varchar(64)                           null comment '实际审批人',
+    approver_user     varchar(64)                           null comment '实际审批人',
     approve_time    datetime                              null comment '实际审批时间',
     approve_comment varchar(512)                          null comment '审批备注',
-    created_at      datetime    default CURRENT_TIMESTAMP not null,
-    updated_at      datetime    default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP,
+    `create_date` datetime DEFAULT CURRENT_TIMESTAMP comment '审批创建时间',
+    `create_by` int(20) DEFAULT null comment '审批创建人id',
+    `create_by_name` varchar(50) COLLATE utf8mb4_bin DEFAULT null comment '审批创建人名称',
+    `update_date` datetime DEFAULT CURRENT_TIMESTAMP,
+    `update_by` int(20) DEFAULT NULL,
+    `update_by_name` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
     constraint uk_approval_no  unique (approval_no)
 )
     comment '财管审批单据表';
@@ -28,10 +30,15 @@ create table rec_finance_approval_approver
     uin          varchar(64)                        not null comment '',
     manager_id   varchar(64)                        not null comment '财管负责人id',
     manager_name varchar(64)                        not null comment '负责人名称',
-    role_title   varchar(64)                        null comment '角色',
-    created_at   datetime default CURRENT_TIMESTAMP not null
+    role_title   varchar(64)                        null comment '财管角色',
+    create_date datetime DEFAULT CURRENT_TIMESTAMP,
+    create_by int(20) DEFAULT NULL,
+    create_by_name varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+    update_date datetime DEFAULT CURRENT_TIMESTAMP,
+    update_by int(20) DEFAULT NULL,
+    update_by_name varchar(50) COLLATE utf8mb4_bin DEFAULT NULL
 )
-    comment '审批人快照表';
+    comment '审批单对应的审批人';
 
 create index idx_approval on rec_finance_approval_approver (approval_id);
 
